1b6295937295146ce0f97671ad4e40d70b8fb3b1
unknown <lqchan022@gmail.com>
2021-08-14 23:24:34 +0800

    Code for testing random stuff, ignore

--- RPI/Temporary code.py
+++ RPI/Temporary code.py
@@ -1,2 +1,18 @@
-dic = {"FRONT":100}
-print(dic.get("FRON", True) > 1)
+class testclass():
+    def __init__(self):
+        self.attr = 10
+    def add2(self):
+        self.attr += 2
+
+a = testclass()
+print(a.attr)
+
+def add2(cls):
+    cls.attr += 2
+    cls.add2()
+
+add2(a)
+
+print(a.attr)
+
+print("Front {}".format(a.attr))
